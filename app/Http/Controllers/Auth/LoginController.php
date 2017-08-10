b2edada9ad34ace4f441c51043555cc8b0bee0bc
kenezo <myafkd@gmail.com>
2017-08-10 08:42:29 +0000

    authentification

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/fatou';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -27,44 +27,7 @@ Route::get('/', function (){
  Route::post('bin', 'ContacController@store');
 
 
-//   //Route::get('contact/show/{id}','contacController@show');
 
-//// les  routes  ogmenté 
+Auth::routes();
 
-
-   // Route::get('/', function () {
-   //      $contact = \App\contact::orderBy('created_at', 'asc')->get();
-   //      return view('contact', [
-   //          'contact' => $contact
-   //      ]);
-   //  });
-
-   //  /**
-   //   * Add New Task
-   //   */
-   //  Route::post('/contact', function (Request $request) {
-   //      $validator = Validator::make($request->all(), [
-   //          'name' => 'required|max:255',
-   //      ]);
-
-   //      // if ($validator->fails()) {
-   //      //     return redirect('/')
-   //      //         ->withInput()
-   //      //         ->withErrors($validator);
-   //      // }
-
-   //      $contact = new \App\contact;
-   //      $contact->name = $request->name;
-   //      $contact->save();
-
-   //      return redirect('/contact');
-   //  });
-
-   //  /**
-   //   * Delete Task
-   //   */
-   //  Route::delete('/contact/{contact}', function (\App\contact $contact) {
-   //      $contact->delete();
-
-   //      return redirect('/');
-   //  });
+Route::get('/home', 'HomeController@index')->name('home');
